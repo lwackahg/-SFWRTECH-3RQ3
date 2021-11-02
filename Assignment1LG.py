291893529020a24e3b0190ab43740e5cd47ed8ed
Gabry <gabryl1@mcmaster.ca>
2021-11-02 14:23:24 -0400

    Assignment1

---
+++ Assignment1LG.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+# Lukas Gabrys
+# ID - 400423742
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Lukas')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
